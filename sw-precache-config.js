8db76af8ad93d6e8bfcb5a4c5ffb1937e0725612
@bkawk <email.will.in.china@gmail.com>
2017-03-31 19:50:22 +0800

    feat: add service worker

---
+++ sw-precache-config.js
@@ -0,0 +1,10 @@
+/* eslint-env node */
+
+module.exports = {
+  staticFileGlobs: [
+    '/index.html',
+    '/manifest.json',
+    '/bower_components/webcomponentsjs/webcomponents-lite.min.js',
+  ],
+  navigateFallback: 'index.html',
+};
